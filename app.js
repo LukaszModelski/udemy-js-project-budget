114cb6bf68b2d779a60bafbf0cb64b5d4d5cb346
Czapa <lukaszmodelski90@gmail.com>
2018-09-09 15:33:30 +0200

    Expanse, INcome function constructors

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
 
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
 })();
 
 // UI CONTROLLER
@@ -31,19 +54,30 @@ var UIController = (function() {
 // MAIN CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDOMStrings();
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMStrings();
+
+        document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {
+            if(event.keyCode === 13 || event.which === 13 ) {
+                ctrlAddItem();
+            }
+        });
+    };
 
     var ctrlAddItem = function() {
-        var input = UIController.getinput();
+        var input = UICtrl.getinput();
         console.log(input);
     }
 
-    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-        if(event.keyCode === 13 || event.which === 13 ) {
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log('test form init');
+            setupEventListeners();
         }
-    });
+    };
 
 })(budgetController, UIController);
+
+controller.init();
